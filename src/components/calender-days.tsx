4cd3e4674ee89e196eeb6591d19edac55eed357a
Dattaraj Dhadas <dhadas808@yahoo.com>
2025-05-09 22:31:42 +0530

    Updates: A: 3 M: 6

--- src/components/calender-days.tsx
+++ src/components/calender-days.tsx
@@ -4,12 +4,12 @@ import { cc } from '../utils/cc'
 import { formatDate } from '../utils/Date-format'
 import { useState } from 'react'
 import ModalForm from './modal-form'
-import { useEvent } from '../hooks/useEvent'
+
 import { CalenderDaysProps } from '../types/calenderDays'
 
 const CalenderDays = ({showWeekName, day,selectedMonth, sortedEvents}: CalenderDaysProps) => {
   const [isNewEventModalOpen, setIsNewEventModalOpen] = useState(false)
-  const { events } = useEvent()
+
   return (
         
         <div className={cc("day", !isSameMonth(day,selectedMonth) && "non-month-day",
@@ -25,22 +25,6 @@ const CalenderDays = ({showWeekName, day,selectedMonth, sortedEvents}: CalenderD
             <button key={event.id} className={cc("event", event.allDay && "all-day-event", event.color)}>
               <div className="event-name">{event.name}</div>
             </button>
-         
-      //   <div className="events">
-      //   <button className="all-day-event blue event">
-      //     <div className="event-name">Short</div>
-      //   </button>
-      //   <button className="all-day-event green event">
-      //     <div className="event-name">
-      //       Long Event Name That Just Keeps Going
-      //     </div>
-      //   </button>
-      //   <button className="event">
-      //     <div className="color-dot blue"></div>
-      //     <div className="event-time">7am</div>
-      //     <div className="event-name">Event Name</div>
-      //   </button> 
-      // </div>
        )
       })
       }
